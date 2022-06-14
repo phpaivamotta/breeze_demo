c3f041931d121b6103f7f75422c00df8cdbf67b6
Pedro Henrique de Paiva Motta <phpaivamotta@gmail.com>
2022-06-13 22:43:32 -0300

    created Job migation controller model and factory

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/', [JobController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
